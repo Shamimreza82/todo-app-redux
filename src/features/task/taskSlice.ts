2d0cc8d1d9e0f512c62ce2263f2ba48745fa5f05
Shamimreza82 <shamimrezaone@gmail.com>
2025-01-06 23:35:44 +0600

    update code

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    task: {
+        id: "12364", 
+        title: "initial frontend", 
+        description: "create home page and routing", 
+        dueDate: "2025-11", 
+        isCompleted: false, 
+        priority: "High"
+    }
+}
+
+
+const taskSlice = createSlice ({
+    name: 'task', 
+    initialState, 
+    reducers: {}
+})
